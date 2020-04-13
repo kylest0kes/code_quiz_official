6ec9f6d37b66e418b312fa1901e00d6019da69b9
kylest0kes <kylestokes1551@gmail.com>
2020-04-12 22:54:25 -0400

    got it so on start button click it begins the questions

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var timeEl = document.querySelector(".timer");
 var startButton = document.getElementById("start-btn");
 var quizStart = document.getElementById("quiz-start")
 var questionContainer = document.getElementById("question-container");
+var questionEl = document.getElementById("question");
+var answerEl = document.getElementById("answers");
+
+
 
 startButton.addEventListener('click', startQuiz)
 
@@ -11,14 +15,29 @@ var secondsLeft = 75;
 function startQuiz(){
     quizStart.classList.add('hide')
     questionContainer.classList.remove('hide')
+    
 }
 
 //function to set the next question
 function nextQuestion (){
+    
+}
 
+//function to show next question
+function showNextQuestion(){
+    q
 }
 
 //function for selecting an answer
 function selectAnswer(){
 
 }
+
+//function to start timer
+function startTimer(){
+
+}
+
+//questions
+
+   
